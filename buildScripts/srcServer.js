1e2794e44260aaca2074010596952dd988bfed2c
Samuel Mwangi Mbuthia <mbuthia.jsamuel@gmail.com>
2019-07-08 10:29:05 +0300

    add transpiling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,8 +1,18 @@
-var express = require ('express'); //call express dependency
-var path = require ('path'); //reference to path
-var open = require ('open'); //open a site in the browser
-var port = 3030; // a variable that stores the port
-var app = express (); // create an instance of express and set that to variable app
+import express from'express'; //call express dependency
+import path from'path'; //reference to path
+import open from 'open';  //open a site in the browser
+import webpack from "webpack";
+import config from '../webpack.config.dev.js';
+
+const port = 3030; // a variable that stores the port
+const app = express (); // create an instance of express and set that to variable app
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler,{
+  noInfo:true,
+  publicPath: config.output.publicPath
+}));
+
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'../src/index.html'));
 });
