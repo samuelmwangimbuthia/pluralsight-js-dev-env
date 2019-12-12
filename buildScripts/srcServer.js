8b77752651a45d526d98d86bf54ec6cc79405f38
samuel mbuthia <mbuthia.jsamuel@gmail.com>
2019-12-12 08:44:24 +0300

    Bundling module

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,25 +1,26 @@
-import express from'express'; //call express dependency
-import path from'path'; //reference to path
-import open from 'open';  //open a site in the browser
-import webpack from "webpack";
-import config from '../webpack.config.dev.js';
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-const port = 3030; // a variable that stores the port
-const app = express (); // create an instance of express and set that to variable app
+const port = 4242;
+const app = express();
 const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler,{
-  noInfo:true,
-  publicPath: config.output.publicPath
+    noInfo: true,
+    publicPath: config.output.publicPath
 }));
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname,'../src/index.html'));
-});
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+}); 
+
 app.listen(port,function(err){
-    if(err){
+    if (err){
         console.log(err);
     }else{
         open('http://localhost:'+port);
     }
-})
+});
